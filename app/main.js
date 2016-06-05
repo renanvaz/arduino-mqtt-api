833bcd09f41c7496df488909069c287410595897
Renan Vaz <renan.c.vaz@gmail.com>
2016-06-05 19:53:46 -0300

    Working...

--- app/main.js
+++
@@ -1,6 +0,0 @@
-import {INPUT, OUTPUT, INPUT_PULLUP, HIGH, LOW, Board} from '../src/Board';
-
-var b = new Board('123.456.1.789');
-b.pinMode(15, INPUT);
-b.pinMode('1', OUTPUT);
-b.pinMode(10, INPUT_PULLUP);
